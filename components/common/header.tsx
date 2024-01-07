6e51c3de989257c28401ead974e65275a2944158
huynvn010193 <hpn010193@gmail.com>
2024-01-07 16:44:06 +0700

    Pending 02-08 - 16:26

---
+++ components/common/header.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export interface HeaderProps {}
+
+export default function Header(props: HeaderProps) {
+  console.log("render heaeder");
+  return <div>Header</div>;
+}
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,46 @@
+// import Header from "@/components/common/header";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+// chỉ render bên phía client. ko render phía server.
+const Header = dynamic(() => import("@/components/common/header"), {
+  ssr: false,
+});
 
 export interface AboutPageProps {}
 
 export default function AboutPage(props: AboutPageProps) {
+  const [postList, setPostList] = useState([]);
   const router = useRouter();
-  console.log("About query", router.query);
-  return <div>AboutPage</div>;
+
+  // Chỉ chạy phía client
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        "https://js-post-api.herokuapp.com/api/posts?_page=1"
+      );
+      const data = await response.json();
+      setPostList(data.data);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h1>About Page</h1>
+      <Header />
+
+      <ul className="post-list">
+        {postList.map((post: any) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export async function getServerSideProps() {
-  return {
-    props: {},
-  };
-}
+// export async function getServerSideProps() {
+//   return {
+//     props: {},
+//   };
+// }
