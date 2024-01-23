323ce0730cedb449ea5b77213f4f2777f0d72234
huynvn <Huy.Nguyen@navigosgroup.com>
2024-01-24 05:52:33 +0700

    test proxy

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -1,13 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import httpProxy from "http-proxy";
 
-type Data = {
-  name: string;
+// type Data = {
+//   name: string;
+// };
+
+const proxy = httpProxy.createProxyServer();
+
+// turn of bodyParser : vì default bodyParser send lên proxy sẽ được parse
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 };
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
-  res.status(200).json({ name: "PATH - match on here" });
+  // don't send cookies to API server
+  req.headers.cookie = "";
+
+  // /api/students
+  // https://js-post-api.herokuapp.com/api/students
+
+  proxy.web(req, res, {
+    target: process.env.API_URL,
+    changeOrigin: true,
+    selfHandleResponse: false,
+  });
+
+  // res.status(200).json({ name: "PATH - match on here" });
 }
