552121594c09dbe2ff6d10f6f559a3c5b40c265e
huynvn010193 <hpn010193@gmail.com>
2024-01-30 08:32:32 +0700

    Pending code

---
+++ pages/login.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+export default function App() {
+  async function handleLoginClick() {}
+
+  async function handleGetProfileClick() {}
+
+  async function handleLogoutClick() {}
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <button onClick={handleLoginClick}>Login</button>
+      <button onClick={handleGetProfileClick}>Get Profile</button>
+      <button onClick={handleLogoutClick}>Logout</button>
+    </div>
+  );
+}
