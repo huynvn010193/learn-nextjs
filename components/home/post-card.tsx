eeb792c5953d1e4eaf7d44eababb12aa211ff821
huynvn010193 <hpn010193@gmail.com>
2024-02-09 16:03:00 +0700

    04-13 Làm giao diện cho section Recent posts - MUI 5

---
+++ components/home/post-card.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { Card, CardContent, Typography } from "@mui/material";
+
+export interface PostCardProps {}
+
+export function PostCard(props: PostCardProps) {
+  return (
+    <Card>
+      <CardContent>
+        <Typography>dsda</Typography>
+        <Typography>dsda</Typography>
+        <Typography>dsda</Typography>
+        <Typography>dsda</Typography>
+        <Typography>dsda</Typography>
+        <Typography>dsda</Typography>
+      </CardContent>
+    </Card>
+  );
+}
--- components/home/recent-post.tsx
+++ components/home/recent-post.tsx
@@ -8,16 +8,46 @@ import {
   Link as MuiLink,
 } from "@mui/material";
 import Link from "next/link";
+import { PostCard } from "./post-card";
 
 export function RecentPosts() {
   return (
-    <Box component="section">
+    <Box component="section" bgcolor="secondary.light" pb={4} pt={2}>
       <Container>
-        <Stack>
-          <Typography>Recent Posts</Typography>
+        <Stack
+          direction="row"
+          mb={2}
+          justifyContent={{ xs: "center", md: "space-between" }}
+          alignItems="center"
+        >
+          <Typography variant="h5">Recent Posts</Typography>
           <Link href="/blog" passHref legacyBehavior>
-            <MuiLink>View All</MuiLink>
+            <MuiLink sx={{ display: { xs: "none", md: "inline-block" } }}>
+              View All
+            </MuiLink>
           </Link>
+        </Stack>
+        <Stack
+          direction={{
+            xs: "column",
+            md: "row",
+          }}
+          spacing={3}
+          sx={{
+            "& > div": {
+              width: {
+                xs: "100%",
+                md: "50%",
+              },
+            },
+          }}
+        >
+          <Box>
+            <PostCard />
+          </Box>
+          <Box>
+            <PostCard />
+          </Box>
         </Stack>
       </Container>
     </Box>
