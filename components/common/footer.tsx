9be2e6232653b6aac4586596f54d579f48706e52
huynvn <Huy.Nguyen@navigosgroup.com>
2024-02-13 19:10:37 +0700

    04-19 Làm giao diện footer Chỉ dành cho hội viên

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,10 +1,33 @@
 import * as React from "react";
-import { Box } from "@mui/material";
+import { Box, Stack, Typography, Icon } from "@mui/material";
+import { Facebook, Instagram, Twitter, LinkedIn } from "@mui/icons-material";
 
 export function Footer() {
+  const socialLinks = [
+    { icon: Facebook, url: "https://google.com/" },
+    { icon: Instagram, url: "https://google.com/" },
+    { icon: Twitter, url: "https://google.com/" },
+    { icon: LinkedIn, url: "https://google.com/" },
+  ];
   return (
     <Box component='footer' py={2} textAlign='center'>
-      Footer
+      <Stack direction='row' justifyContent='center'>
+        {socialLinks.map((item, idx) => (
+          <Box
+            key={idx}
+            component='a'
+            p={2}
+            href={item.url}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <Icon component={item.icon} sx={{ fontSize: 48 }} />
+          </Box>
+        ))}
+      </Stack>
+      <Typography>
+        Copyright © {new Date().getFullYear()} All rights reserved.
+      </Typography>
     </Box>
   );
 }
