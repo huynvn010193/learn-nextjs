df7331ad8eb2fffaa8524d2a2be6fe2e8087abae
huynvn <Huy.Nguyen@navigosgroup.com>
2024-02-15 14:17:01 +0700

    04-20 Hỗ trợ SEO cho trang Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,21 @@ import { MainLayout } from "@/components/layout";
 import { NextPageWithLayout } from "../models";
 import { Box } from "@mui/material";
 import { FeaturedWorks, HeroSection, RecentPosts } from "@/components/home";
+import { SEO } from "@/components/common";
 
 const Home: NextPageWithLayout = () => {
   const router = useRouter();
 
   return (
     <Box>
+      <SEO
+        data={{
+          title: "NextJS learn",
+          description: "NextJS learn",
+          url: "https://vercel.com/huynvn010193/learn-nextjs",
+          thumbnail: "",
+        }}
+      />
       <HeroSection />
       <RecentPosts />
       <FeaturedWorks />
