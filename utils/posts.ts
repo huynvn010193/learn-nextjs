51444886989c43469a67a0d5e0a0fa0f0c0e8562
huynvn <Huy.Nguyen@navigosgroup.com>
2024-02-29 11:02:00 +0700

    05-03 Đọc nội dung của tất cả file markdown

--- utils/posts.ts
+++ utils/posts.ts
@@ -5,5 +5,13 @@ import fs from "fs";
 const BLOG_FOLDER = path.join(process.cwd(), "blog");
 
 export async function getPostList() {
+  // read all markdown files
+  const fileNameList = fs.readdirSync(BLOG_FOLDER);
+
+  for (const fileName of fileNameList) {
+    const filePath = path.join(BLOG_FOLDER, fileName);
+    const fileContents = fs.readFileSync(filePath, "utf-8");
+  }
+
   return [];
 }
